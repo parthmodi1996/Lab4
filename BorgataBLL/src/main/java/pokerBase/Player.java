58a2c56ecb2506345c2c505555f00c5bc4e7ed99
parthmodi1996 <parth@udel.edu>
2015-11-02 15:36:07 -0500

    Initial Commit

---
+++ BorgataBLL/src/main/java/pokerBase/Player.java
@@ -0,0 +1,14 @@
+package pokerBase;
+
+import domain.PlayerDomainModel;
+
+public class Player extends PlayerDomainModel {
+
+	public Player(String PlayerName, int iPlayerPosition) {
+		super(PlayerName, iPlayerPosition);
+	}
+
+	public int getiPlayerPosition() {
+		return iPlayerPosition;
+	}	
+}
